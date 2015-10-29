11bfb27e2054e8842ead449d27657f23066cc399
abudnikov-q <abudnikov@qulix.com>
2015-10-29 19:58:06 +0300

    refactored homework04

--- homework04/src/test/java/ru/itsphere/jrpadawan/clazz/ClassTest.java
+++ homework04/src/test/java/ru/itsphere/jrpadawan/clazz/ClassTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.ParameterizedType;
 
 public class ClassTest {
 
@@ -19,57 +17,74 @@ public class ClassTest {
 
     @Test
     public void test1() {
-        Class<?> clazz = null;
+        Class<?> userClass = checkClassExistence();
+
+        Object newInstance = checkInstanceCreationWithDefaultConstructor(userClass);
+        Field idField = checkFieldExistance(userClass, FIELD_ID);
+        Field ageField = checkFieldExistance(userClass, FIELD_AGE);
+
+        checkValueField(userClass, newInstance, idField, 0);
+        checkValueField(userClass, newInstance, ageField, 0);
+
+        Constructor<?> twoArgumentConstructor = checkTwoArgumentConstructor(userClass);
+        Object initializedInstance = checkCreationInstanceWithTwoTestParams(userClass, twoArgumentConstructor);
+        checkValueField(userClass, initializedInstance, idField, TEST_USER_ID);
+        checkValueField(userClass, initializedInstance, ageField, TEST_USER_AGE);
+    }
+
+    private Constructor<?> checkTwoArgumentConstructor(Class<?> clazz) {
         try {
-            clazz = Class.forName(USER_CLASS, INITIALIZE, ClassLoader.getSystemClassLoader());
-            Assert.assertNotNull(clazz);
-            Object newInstance = clazz.newInstance();
-            checkValueField(clazz, newInstance, FIELD_ID, 0);
-            checkValueField(clazz, newInstance, FIELD_AGE, 0);
-            checkClassConstructor(clazz);
+            return clazz.getConstructor(Integer.class, Integer.class);
+        } catch (NoSuchMethodException e) {
+            Assert.fail("Object of class " + USER_CLASS + ". Two Argument constructor access error.");
+        }
+        return null;
+    }
+
+    private Object checkInstanceCreationWithDefaultConstructor(Class<?> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException e) {
+            Assert.fail("Object of class " + USER_CLASS + " was not created");
+        } catch (IllegalAccessException e) {
+            Assert.fail("Object of class " + USER_CLASS + ". Default constructor access error.");
+        }
+        return null;
+    }
+
+    private Class<?> checkClassExistence() {
+        try {
+            Class<?> userClass = Class.forName(USER_CLASS, INITIALIZE, ClassLoader.getSystemClassLoader());
+            Assert.assertNotNull(userClass);
+            return userClass;
         } catch (ClassNotFoundException e) {
             Assert.fail("Class " + USER_CLASS + " was not created");
-        } catch (InstantiationException e) {
-            Assert.fail("Class " + USER_CLASS + " was not created");
-        } catch (IllegalAccessException e) {
-            Assert.fail("Class " + USER_CLASS + " was not created");
         }
+        return null;
     }
 
-    private void checkClassConstructor(Class<?> clazz) {
+    private Object checkCreationInstanceWithTwoTestParams(Class<?> clazz, Constructor<?> constructor) {
+        Object instance = null;
         try {
-            Object userInstanceWithValues = getUserWithTwoTestParams(clazz);
-        } catch (NoSuchMethodException e) {
-            Assert.fail("Constructor with two parameters was not found");
-        }
-        checkValueField(clazz, userInstanceWithValues, FIELD_ID, TEST_USER_ID);
-        checkValueField(clazz, userInstanceWithValues, FIELD_AGE, TEST_USER_AGE);
-    }
-
-    private Object getUserWithTwoTestParams(Class<?> clazz) throws NoSuchMethodException {
-        Constructor<?> twoParamsConstructor = clazz.getConstructor(Integer.class, Integer.class);
-        Object userInstanceWithValues = null;
-        try {
-            userInstanceWithValues = twoParamsConstructor.newInstance(TEST_USER_ID, TEST_USER_AGE);
+            instance = constructor.newInstance(TEST_USER_ID, TEST_USER_AGE);
         } catch (Exception e) {
             Assert.fail("Constructor with two parameters is incorrect");
         }
-        return userInstanceWithValues;
+        return instance;
     }
 
-    private void checkValueField(Class<?> clazz, Object newInstance, String fieldName, Object value) {
-        Field fieldId = getField(clazz, fieldName);
+    private void checkValueField(Class<?> clazz, Object newInstance, Field field, Object value) {
         try {
-            Object valueActual = fieldId.get(newInstance);
+            Object valueActual = field.get(newInstance);
             if (!value.equals(valueActual)) {
-                Assert.fail("Field " + fieldName + " has incorrect value");
+                Assert.fail("Field " + field.getName() + " has incorrect value");
             }
         } catch (IllegalAccessException e) {
-            Assert.fail("Field " + fieldName + " can't be accessed");
+            Assert.fail("Field " + field.getName() + " can't be accessed");
         }
     }
 
-    private Field getField(Class<?> clazz, String fieldName) {
+    private Field checkFieldExistance(Class<?> clazz, String fieldName) {
         try {
             return clazz.getDeclaredField(fieldName);
         } catch (NoSuchFieldException e) {
