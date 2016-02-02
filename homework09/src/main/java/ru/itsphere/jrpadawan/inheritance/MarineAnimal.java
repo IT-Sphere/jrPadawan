6b7de12d96b13e61668dc93a9dfa8757f44e3486
Ivan Yarosh <rat90@inbox.ru>
2016-02-02 20:57:33 +0300

    -

---
+++ homework09/src/main/java/ru/itsphere/jrpadawan/inheritance/MarineAnimal.java
@@ -0,0 +1,26 @@
+package ru.itsphere.jrpadawan.inheritance;
+
+/**
+ * Абстрактный класс. Морское животное.
+ */
+public abstract class MarineAnimal extends Animal {
+    /**
+     * Скорость в воде.
+     */
+    private double swimSpeed;
+
+    /**
+     * Движение в воде
+     */
+    public double move(double distance) {
+        return distance / swimSpeed;
+    }
+
+    public double getSwimSpeed() {
+        return swimSpeed;
+    }
+
+    public void setSwimSpeed(double swimSpeed) {
+        this.swimSpeed = swimSpeed;
+    }
+}
---
+++ homework09/src/main/java/ru/itsphere/jrpadawan/inheritance/Whale.java
@@ -0,0 +1,8 @@
+package ru.itsphere.jrpadawan.inheritance;
+
+public class Whale extends MarineAnimal {
+    public Whale(String name) {
+        setName(name);
+        setSwimSpeed(10.3);
+    }
+}
